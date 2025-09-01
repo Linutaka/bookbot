f5b9208f83e03a366b9ae230c9eb93fd81e27289
Linutaka <robertalexandermueller1@gmail.com>
2025-09-01 15:54:36 +0200

    added reading and printing text, added word count off file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as file:
+        file_contents = file.read()
+    return file_contents
+
+def num_words(words):
+    words_list = words.split()
+    return len(words_list)
+
+def main():
+    book = get_book_text("/home/robert/projects/bookbot/books/frankenstein.txt")
+    print(book)
+
+    words = num_words(book)
+    print(f"{words} words found in the document")
+
+
+main()
